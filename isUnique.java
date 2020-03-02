9b69e8959ac0cb3d26da6e22bca20aa5912c4126
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-01 19:08:10 -0800

    Add files via upload

--- isUnique.java
+++ isUnique.java
@@ -10,19 +10,23 @@ import java.util.Arrays;
 public class isUnique {
 
 	public static void main(String[] args) {
-		int[] list = {2, 9, 17, 43, -8, 46};
+		int[] list = { 2, 9, 17, 43, -8, 46 };
 		int[] list1 = { 4, 7, 3, 9, 12, -47, 3, 74 };
+		int[] list2 = { 5, -5, 5, 0 };
+		int[] list3 = { 10, -5, -2, 0 };
 		System.out.println(isUnique(list));
 		System.out.println(isUnique(list1));
+		System.out.println(isUnique(list2));
+		System.out.println(isUnique(list3));
 
 	}
 
 	public static boolean isUnique(int[] list) {
 		for (int i = 0; i < list.length; i++) {
 			int prev = list[i];
-			System.out.println("prev  " + prev);
+//			System.out.println("prev  " + prev);
 			for (int k = i + 1; k < list.length; k++) {
-				System.out.println("K  " + list[k]);
+//				System.out.println("K  " + list[k]);
 				if (prev == list[k]) {
 					return false;
 				}

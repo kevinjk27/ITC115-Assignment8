5d4fbb7bf6aa1005f5bba3bf9e48238ff97e2019
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-01 19:02:18 -0800

    Checking whether numbers in an arrays are unique

---
+++ isUnique.java
@@ -0,0 +1,33 @@
+/*
+ * Kevin Kantono
+ * 02/29/2020
+ * Checking whether numbers in an arrays are unique.
+ */
+
+import java.awt.List;
+import java.util.Arrays;
+
+public class isUnique {
+
+	public static void main(String[] args) {
+		int[] list = {2, 9, 17, 43, -8, 46};
+		int[] list1 = { 4, 7, 3, 9, 12, -47, 3, 74 };
+		System.out.println(isUnique(list));
+		System.out.println(isUnique(list1));
+
+	}
+
+	public static boolean isUnique(int[] list) {
+		for (int i = 0; i < list.length; i++) {
+			int prev = list[i];
+			System.out.println("prev  " + prev);
+			for (int k = i + 1; k < list.length; k++) {
+				System.out.println("K  " + list[k]);
+				if (prev == list[k]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
